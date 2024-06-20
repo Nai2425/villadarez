7771932a6d9a85b2f5b7da6ba93d956ce1af60ae
Ian Beach Villadarez <ianbeach.villadarez025@gmail.com>
2024-06-21 00:15:58 +0800

    hakdog

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Index />} />
+        <Route path="/villadarez" element={<Index />} />
       </Routes>
     </>
   );
--- src/template/Index.jsx
+++ src/template/Index.jsx
@@ -105,7 +105,7 @@ const Index = () => {
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 "
             >
               <li>
-                <a href="/" className="text-lg">
+                <a href="/villadarez" className="text-lg">
                   <FontAwesomeIcon icon={faHouse} /> Home
                 </a>
               </li>
